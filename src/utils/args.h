b9b3a703b0b130ebd131338ab97bde9f3de4dcb3
Przemyslaw Rokosz <przemyslaw.rokosz@gmail.com>
2024-08-11 11:22:36 +0200

    arguments parsing implementation (#1)

---
+++ src/utils/args.h
@@ -0,0 +1,18 @@
+#ifndef ARGS_H
+#define ARGS_H
+
+namespace vgraph {
+namespace utils {
+
+struct args {
+    bool help = false;
+    bool debug = false;
+
+    static args parse(int argc, char* argv[]);
+    static void print_help();
+};
+
+} // namespace utils
+} // namespace vgraph
+
+#endif // ARGS_H
