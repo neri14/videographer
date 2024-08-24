5f62ce361a37ae85c933b66b2d2088e33ac385b4
Przemyslaw Rokosz <przemyslaw.rokosz@gmail.com>
2024-08-24 14:46:51 +0200

    integrated gstreamer video generation

--- src/video/generator.cpp
+++ src/video/generator.cpp
@@ -1,4 +1,9 @@
 #include "generator.h"
+#include "pipeline.h"
+
+extern "C" {
+    #include <gst/gst.h>
+}
 
 namespace vgraph {
 namespace video {
@@ -11,6 +16,32 @@ generator::generator(const std::string& input, const std::string& output):
 void generator::generate()
 {
     log.info("Starting video generation");
+
+    log.info("Initializing Gstreamer");
+    gst_init(nullptr, nullptr);
+
+    log.info("Creating pipeline");
+    pipeline pipe(input_path_, output_path_);
+
+    if (!pipe.build()) {
+        log.error("Pipeline creation failed");
+        return;
+    }
+    
+    if (!pipe.link()) {
+        log.error("Pipeline static linking failed");
+        return;
+    }
+
+    if (!pipe.init()) {
+        log.error("Pipeline initialization failed");
+        return;
+    }
+
+    if (!pipe.run()) {
+        log.error("Pipeline execution failed");
+        return;
+    }
 
     log.info("Finished video generation");
 }
---
+++ src/video/pipeline.h
@@ -0,0 +1,41 @@
+#ifndef PIPELINE_H
+#define PIPELINE_H
+
+#include "utils/logging/logger.h"
+
+#include <map>
+#include <string>
+
+extern "C" {
+    #include <gst/gst.h>
+}
+
+namespace vgraph {
+namespace video {
+
+class pipeline {
+public:
+    pipeline(const std::string& input_path, const std::string& output_path);
+    ~pipeline();
+
+    bool build();
+    bool link();
+    bool init();
+    bool run();
+
+    //FIXME to check if canbe reworked and privatized
+    void pad_added_handler(GstElement* src, GstPad* new_pad);
+
+private:
+    bool link_elements(const std::string& src, const std::string& dest, GstCaps* filter = nullptr);
+
+    utils::logging::logger log{"pipeline"};
+    std::map<std::string, GstElement*> elements;
+    std::string input_path_;
+    std::string output_path_;
+};
+
+} // namespace video
+} // namespace vgraph
+
+#endif // PIPELINE_H
