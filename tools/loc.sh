4d8bcc722bbedc33e7b867ce49779ea56f73eaff
Przemyslaw Rokosz <przemyslaw.rokosz@gmail.com>
2024-08-26 18:42:14 +0200

    update LOC script, exclude .vscode dir

--- tools/loc.sh
+++ tools/loc.sh
@@ -2,5 +2,5 @@
 
 project_dir=$(dirname $(dirname $(realpath $0)))
 
-cloc --exclude-dir 3rdparty --exclude-ext gpx,fit $project_dir
+cloc --exclude-dir 3rdparty,.vscode --exclude-ext gpx,fit $project_dir
 
