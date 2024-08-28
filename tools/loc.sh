00d66e02d60671b5cc2aa771f66ff5c7c6f19eb3
Przemyslaw Rokosz <przemyslaw.rokosz@gmail.com>
2024-08-28 15:34:57 +0200

    added build to ignored directories in LOC counting

--- tools/loc.sh
+++ tools/loc.sh
@@ -2,5 +2,5 @@
 
 project_dir=$(dirname $(dirname $(realpath $0)))
 
-cloc --exclude-dir 3rdparty,.vscode --exclude-ext gpx,fit $project_dir
+cloc --exclude-dir 3rdparty,.vscode,build --exclude-ext gpx,fit $project_dir
 
