59c424b55d2d8d0b79277044591efe3f61865135
Przemyslaw Rokosz <przemyslaw.rokosz@gmail.com>
2024-09-01 10:22:04 +0200

    config file parsing

--- src/arguments.cpp
+++ src/arguments.cpp
@@ -1,12 +1,17 @@
 #include "arguments.h"
 #include "utils/argument_parser.h"
 
+#include <fstream>
+#include <sstream>
+
 namespace vgraph {
 namespace consts {
-    int defult_alignment_clip_time(60);
+    const int defult_alignment_clip_time(60);
+    const std::string whitespaces(" \t\v\r\n");
 }
 namespace key {
     std::string help("help");
+    std::string config("config");
     std::string debug("debug");
     std::string gpu("gpu");
     std::string timecode("timecode");
@@ -25,12 +30,76 @@ namespace key {
 utils::argument_parser prepare_parser();
 
 arguments read_args(const utils::argument_parser& parser, utils::logging::logger& log);
+
+bool parse_config_file(const std::string& path, arguments& args);
+
 void assert_arguments_valid(const arguments& args, utils::logging::logger& log);
 
 template <typename T>
 bool read_value(const utils::argument_parser& parser, const std::string& name, utils::logging::logger& log, std::optional<T>& value_out);
 
 bool parse_resolution(const std::string& str, utils::logging::logger& log, std::optional<std::pair<int, int>>& out);
+
+/* specialized parse_value functions */
+template <typename T>
+bool parse_value(const std::string& str, utils::logging::logger& log, T& value_out)
+{
+    log.error("Unsupported value type");
+    return false;
+}
+
+template <>
+bool parse_value<bool>(const std::string& str, utils::logging::logger& log, bool& value_out)
+{
+    if (str == "true") {
+        value_out = true;
+        return true;
+    } else if (str == "false") {
+        value_out = false;
+        return true;
+    }
+
+    return false;
+}
+
+template <>
+bool parse_value<std::optional<std::string>>(const std::string& str, utils::logging::logger& log, std::optional<std::string>& value_out)
+{
+    value_out = str;
+    return true;
+}
+
+template <>
+bool parse_value<std::optional<int>>(const std::string& str, utils::logging::logger& log, std::optional<int>& value_out)
+{
+    try {
+        value_out = std::stoi(str);
+    } catch (std::invalid_argument) {
+        log.error("Could not parse value '{}' as integer point number", str);
+        return false;
+    } catch (std::out_of_range) {
+        log.error("Value '{}' is out of range", str);
+        return false;
+    }
+
+    return true;
+}
+
+template <>
+bool parse_value<std::optional<double>>(const std::string& str, utils::logging::logger& log, std::optional<double>& value_out)
+{
+    try {
+        value_out = std::stod(str);
+    } catch (std::invalid_argument) {
+        log.error("Could not parse value '{}' as floating point number", str);
+        return false;
+    } catch (std::out_of_range) {
+        log.error("Value '{}' is out of range", str);
+        return false;
+    }
+
+    return true;
+}
 
 /* interface */
 arguments arguments::parse(int argc, char* argv[])
@@ -69,9 +138,10 @@ utils::argument_parser prepare_parser()
     utils::argument_parser parser("vgraph");
 
     parser.add_argument(key::help,       utils::argument().flag().option("-h").option("--help")      .description("Print this help message"));
+    parser.add_argument(key::config,     utils::argument()       .option("-c").option("--config")    .description("Load config file"));
     parser.add_argument(key::debug,      utils::argument().flag().option("-d").option("--debug")     .description("Enable debug logs"));
     parser.add_argument(key::gpu,        utils::argument().flag().option("-g").option("--gpu")       .description("Use Nvidia GPU for processing"));
-    parser.add_argument(key::timecode,   utils::argument().flag().option("-c").option("--timecode")  .description("Draw a timecode on each frame"));
+    parser.add_argument(key::timecode,   utils::argument().flag().option("-m").option("--timecode")  .description("Draw a timecode on each frame"));
     parser.add_argument(key::alignment,  utils::argument().flag().option("-l").option("--alignment") .description(std::format("Enable alignment mode ({}s clip unless overriden by -T)", consts::defult_alignment_clip_time)));
     parser.add_argument(key::telemetry,  utils::argument()       .option("-t").option("--telemetry") .description("Telemetry file path"));
     parser.add_argument(key::layout,     utils::argument()       .option("-a").option("--layout")    .description("Layout file path"));
@@ -89,6 +159,10 @@ arguments read_args(const utils::argument_parser& parser, utils::logging::logger
 {
     arguments a;
     bool valid = true;
+
+    if (parser.has(key::config)) {
+        valid = parse_config_file(parser.get<std::string>(key::config), a);
+    }
 
     a.debug = parser.get<bool>(key::debug);
     a.gpu = parser.get<bool>(key::gpu);
@@ -117,6 +191,64 @@ arguments read_args(const utils::argument_parser& parser, utils::logging::logger
     }
 
     return std::move(a);
+}
+
+/* Expected format:
+#comments are ignored
+key=value
+key=value
+key=value
+ */
+bool parse_config_file(const std::string& path, arguments& args)
+{
+    utils::logging::logger log{"arguments::parse_config_file()"};
+
+    std::ifstream file(path);
+    if (!file.is_open()) {
+        log.error("Failed to open '{}' file", path);
+        return false;
+    }
+
+    std::string line;
+    while(getline(file, line)) {
+        line = line.substr(line.find_first_not_of(consts::whitespaces)); //remove leading whitespaces
+        if (line.starts_with('#')) {
+            continue;
+        }
+
+        int pos = line.find('=');
+        const std::string& key = line.substr(0, pos);
+        const std::string& value = line.substr(pos+1);
+        
+        log.debug("key: '{}' value: '{}'", key, value);
+        if (key == key::debug) {
+            parse_value(value, log, args.debug);
+        } else if (key == key::gpu) {
+            parse_value(value, log, args.gpu);
+        } else if (key == key::timecode) {
+            parse_value(value, log, args.timecode);
+        } else if (key == key::alignment) {
+            parse_value(value, log, args.alignment_mode);
+        } else if (key == key::telemetry) {
+            parse_value(value, log, args.telemetry);
+        } else if (key == key::layout) {
+            parse_value(value, log, args.layout);
+        } else if (key == key::input) {
+            parse_value(value, log, args.input);
+        } else if (key == key::output) {
+            parse_value(value, log, args.output);
+        } else if (key == key::offset) {
+            parse_value(value, log, args.offset);
+        } else if (key == key::resolution) {
+            parse_resolution(value, log, args.resolution);
+        } else if (key == key::bitrate) {
+            parse_value(value, log, args.bitrate);
+        } else if (key == key::clip_time) {
+            parse_value(value, log, args.clip_time);
+        }
+    }
+
+    return true;
 }
 
 void assert_arguments_valid(const arguments& args, utils::logging::logger& log)
@@ -177,7 +309,5 @@ bool parse_resolution(const std::string& str, utils::logging::logger& log, std::
     }
     return true;
 }
-
-
 
 }
